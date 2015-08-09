34bdfff61ae753cdf2258eb3e358b449f2d726fc
FICTURE7 <FICTURE7@gmail.com>
2015-08-09 11:04:46 +0400

    Fixed errors

--- CoCSharp.Server/Program.cs
+++ CoCSharp.Server/Program.cs
@@ -12,7 +12,7 @@ namespace CoCSharp.Server
     {
         public static CoCServer Server { get; set; }
 
-        public static void m(string[] args)
+        public static void Main(string[] args)
         {
             Console.Title = "CoC# Server";
 
@@ -20,12 +20,6 @@ namespace CoCSharp.Server
             Server.Start(new IPEndPoint(IPAddress.Any, 9339));
 
             Thread.Sleep(-1);
-        }
-
-        public static void Main()
-        {
-            var assetManager = new AssetManager(new Fingerprint(File.ReadAllText("fingerprint.json")));
-            assetManager.DownloadAssets("http://b46f744d64acd2191eda-3720c0374d47e9a0dd52be4d281c260f.r11.cf2.rackcdn.com/");
         }
     }
 }
