fb303d3b82f73d703139ba255572afb6a70fa9f6
FICTURE7 <FICTURE7@gmail.com>
2015-08-23 18:35:20 +0400

    Added AvatarRankListRequestPacket

---
+++ CoCSharp/Networking/Packets/AvatarRankListRequestPacket.cs
@@ -0,0 +1,22 @@
+﻿namespace CoCSharp.Networking.Packets
+{
+    public class AvatarRankListRequestPacket : IPacket
+    {
+        public ushort ID { get { return 0x3844; } }
+
+        public byte Unknown1;
+        public long UserID;
+
+        public void ReadPacket(PacketReader reader)
+        {
+            Unknown1 = reader.ReadByte();
+            UserID = reader.ReadInt64();
+        }
+
+        public void WritePacket(PacketWriter writer)
+        {
+            writer.WriteByte(Unknown1);
+            writer.WriteInt64(UserID);
+        }
+    }
+}
