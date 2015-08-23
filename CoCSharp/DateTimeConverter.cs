8a139e851fd3a259eab5ea0dab53f629815557c8
FICTURE7 <FICTURE7@gmail.com>
2015-08-23 11:42:17 +0400

    Added DateTimeConverter.cs

---
+++ CoCSharp/DateTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CoCSharp
+{
+    internal class DateTimeConverter
+    {
+        private static DateTime UnixTime = new DateTime(1970, 1, 1);
+
+        public static DateTime FromUnixTimestamp(double unixTimestamp)
+        {
+            return UnixTime.AddSeconds(unixTimestamp);
+        }
+
+        public static double ToUnixTimestamp(DateTime time)
+        {
+            return (time - UnixTime).TotalSeconds;
+        }
+
+        public static DateTime FromJavaTimestamp(double javaTimestamp)
+        {
+            return UnixTime.AddSeconds(javaTimestamp / 1000);
+        }
+
+        public static double ToJavaTimestamp(DateTime time)
+        {
+            return (time - UnixTime).TotalSeconds * 1000;
+        }
+    }
+}
