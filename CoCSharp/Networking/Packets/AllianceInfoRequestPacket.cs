195282ead7a339fd295654db5f8185a06825e643
FICTURE7 <FICTURE7@gmail.com>
2015-08-23 15:28:56 +0400

    Added AllianceInfoRequestPacket

---
+++ CoCSharp/Networking/Packets/AllianceInfoRequestPacket.cs
@@ -0,0 +1,19 @@
+﻿namespace CoCSharp.Networking.Packets
+{
+    public class AllianceInfoRequestPacket : IPacket
+    {
+        public ushort ID { get { return 0x37DE; } }
+
+        public long ClanID;
+
+        public void ReadPacket(PacketReader reader)
+        {
+            ClanID = reader.ReadInt64();
+        }
+
+        public void WritePacket(PacketWriter writer)
+        {
+            writer.WriteInt64(ClanID);
+        }
+    }
+}
