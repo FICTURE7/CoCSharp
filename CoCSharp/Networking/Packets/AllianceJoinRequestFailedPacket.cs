270703ee9c94706fe4c4fb117f57e591082f6607
FICTURE7 <FICTURE7@gmail.com>
2015-08-27 15:07:56 +0400

    Added AllianceJoinRequestFailedPacket

---
+++ CoCSharp/Networking/Packets/AllianceJoinRequestFailedPacket.cs
@@ -0,0 +1,17 @@
+﻿namespace CoCSharp.Networking.Packets
+{
+    public class AllianceJoinRequestFailedPacket : IPacket
+    {
+        public ushort ID { get { return 0x5EFF; } }
+
+        public void ReadPacket(PacketReader reader)
+        {
+            // Space
+        }
+
+        public void WritePacket(PacketWriter writer)
+        {
+            // Space
+        }
+    }
+}
