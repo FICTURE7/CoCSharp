e541329ea4de0e718a8a785a0ab9ef2609fdb5d3
FICTURE7 <FICTURE7@gmail.com>
2015-09-17 17:06:11 +0400

    ExamplePlugin -> CoCSharp.Client.ExamplePlugin

---
+++ CoCSharp.Client.ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,35 @@
+﻿using CoCSharp.Client.API;
+using System;
+using CoCSharp.Client.API.Events;
+
+namespace ExamplePlugin
+{
+    public class ExamplePlugin : Plugin
+    {
+        public ExamplePlugin()
+        {  
+            // Space
+        }
+
+        public override string Name { get { return "Example Plugin"; } }
+        public override string Description { get { return "A plugin just for sample."; } }
+        public override string Author { get { return "FICTURE7"; } }
+
+        public override void OnLoad()
+        {
+            Client.ChatMessage += OnChatMessage;
+            Client.Login += OnLogin;
+            Console.WriteLine("Plugin loaded!", Name);
+        }
+
+        private void OnLogin(object sender, LoginEventArgs e)
+        {
+            Console.WriteLine("Logged in!");
+        }
+
+        private void OnChatMessage(object sender, ChatMessageEventArgs e)
+        {
+            Console.WriteLine("Got message!\n{0}", e.Message);
+        }
+    }
+}
