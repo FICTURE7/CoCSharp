4be5cb89b8aeab556ee0ac1294902dfb2dc573ae
FICTURE7 <FICTURE7@gmail.cocom>
2015-11-07 12:50:18 +0400

    Removed useless function

--- CoCSharp/Extensions.cs
+++ CoCSharp/Extensions.cs
@@ -33,26 +33,4 @@ namespace CoCSharp
             builder.Append(value.ToString());
         }
     }
-
-
-    internal static class ListExtension
-    {
-        public static DataTable ToDataTable<T>(this IList<T> data)
-        {
-            //converts a List of <T> to DataTable for easier packet implementation
-            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
-            DataTable table = new DataTable();
-            foreach (PropertyDescriptor prop in properties)
-                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
-            foreach (T item in data)
-            {
-                DataRow row = table.NewRow();
-                foreach (PropertyDescriptor prop in properties)
-                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
-                table.Rows.Add(row);
-            }
-            return table;
-        }
-    }
-
 }
