acba4d6526202aa8bfdd00b06c3a51f51f40fc25
FICTURE7 <FICTURE7@gmail.cocom>
2016-04-11 21:14:33 +0400

    Cleaned test code

--- CoCSharp.Server/Program.cs
+++ CoCSharp.Server/Program.cs
@@ -12,7 +12,7 @@ namespace CoCSharp.Server
     {
         public static CoCServer Server { get; set; }
 
-        public static void m(string[] args)
+        public static void Main(string[] args)
         {
             Console.Title = "CoC# - Server";
 
@@ -28,25 +28,6 @@ namespace CoCSharp.Server
 
             Console.WriteLine("Done({0}ms)! Listening on *:9339", stopwatch.Elapsed.TotalMilliseconds);
             Thread.Sleep(Timeout.Infinite);
-        }
-
-        public static void Main()
-        {
-            var downloader = new AssetDownloader("9d075b9546211da641d06f4c576aa9b9c62212fb");
-            downloader.DownloadProgressChanged += Downloader_DownloadProgressChanged;
-            downloader.DownloadCompleted += Downloader_DownloadCompleted; ;
-            downloader.DownloadAssets(string.Empty, true);
-            Console.ReadLine();
-        }
-
-        private static void Downloader_DownloadCompleted(object sender, AssetDownloadCompletedEventArgs e)
-        {
-            Console.WriteLine("Done!");
-        }
-
-        private static void Downloader_DownloadProgressChanged(object sender, AssetDownloadProgressChangedEventArgs e)
-        {
-            Console.WriteLine("Downloaded: {0}% -> {1}", Math.Round(e.ProgressPercentage, 1), e.FileDownloaded.Path);
         }
     }
 }
