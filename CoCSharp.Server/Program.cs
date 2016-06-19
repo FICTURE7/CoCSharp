ee1d467e64e8b464f1ed748c64daa70e1f5ce66a
FICTURE7 <ficture7@gmail.com>
2016-06-19 09:13:25 +0400

    Removed test code

--- CoCSharp.Server/Program.cs
+++ CoCSharp.Server/Program.cs
@@ -1,7 +1,5 @@
-﻿using CoCSharp.Logic;
-using System;
+﻿using System;
 using System.Diagnostics;
-using System.IO;
 using System.Threading;
 
 namespace CoCSharp.Server
@@ -10,7 +8,7 @@ namespace CoCSharp.Server
     {
         public static CoCServer Server { get; set; }
 
-        public static void m(string[] args)
+        public static void Main(string[] args)
         {
             Console.Title = "CoC# - Server";
 
@@ -26,24 +24,6 @@ namespace CoCSharp.Server
 
             Console.WriteLine("Done({0}ms)! Listening on *:9339", stopwatch.Elapsed.TotalMilliseconds);
             Thread.Sleep(Timeout.Infinite);
-        }
-
-        public static void Main()
-        {
-            var villageJson = File.ReadAllText("village.json");
-
-            var sw = new Stopwatch();
-
-            sw.Start();
-            var village = Village.FromJson(villageJson);
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
-
-            sw.Start();
-            var json = village.ToJson();
-            sw.Stop();
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds);
-            Console.ReadLine();
         }
     }
 }
