b5f0e622060cc5de23bfa363c899fc77f3a60eb7
FICTURE7 <ficture7@gmail.com>
2016-07-25 12:01:25 +0400

    Fixed ComponentTests

--- CoCSharp.Test/Logic/ComponentTests.cs
+++ CoCSharp.Test/Logic/ComponentTests.cs
@@ -11,14 +11,30 @@ namespace CoCSharp.Test.Logic
     {
         public ComponentTests()
         {
-            _village = new Village();
+            _manager = new AssetManager(TestUtils.CsvDirectory);
+            _manager.LoadCsv<BuildingData>("buildings.csv");
+            _manager.LoadCsv<ObstacleData>("obstacles.csv");
+            _manager.LoadCsv<TrapData>("traps.csv");
+            _manager.LoadCsv<DecorationData>("decos.csv");
 
-            var thdata = AssetManager.DefaultInstance.SearchCsv<BuildingData>(1000001, 0);
-            _building = new Building(_village, thdata);
+            _village = new Village(_manager);
+
+            // Level 0(1), Town Hall.
+            var thdata = _manager.SearchCsv<BuildingData>(1000001, 0);
+            var th = new Building(_village, thdata);
         }
 
+        private readonly AssetManager _manager;
         private readonly Village _village;
         private Building _building;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Level 0(1), Gold Mine.
+            var data = _manager.SearchCsv<BuildingData>(1000004, 0);
+            _building = new Building(_village, data);
+        }
 
         [Test]
         public void GetComponent_ComponentNotFound_ReturnsNull()
