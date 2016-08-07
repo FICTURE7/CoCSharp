39cb68b524d11ff3893e7a973c348c71b29c11fc
Knight King <kien62248@gmail.com>
2016-08-07 12:59:25 +0700

    Preparing for donation

---
+++ CoCSharp/Network/Messages/Commands/DonateAllianceUnitsCommand.cs
@@ -0,0 +1,42 @@
+﻿namespace CoCSharp.Network.Messages.Commands
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DonateAllianceUnitsCommand : Command
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DonateAllianceUnitsCommand()
+        {
+            // Space
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public override int ID
+        {
+           get
+           {
+                return 22;
+           }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reader"></param>
+        public override void ReadCommand(MessageReader reader)
+        {
+           
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="writer"></param>
+        public override void WriteCommand(MessageWriter writer)
+        {
+            
+        }
+    }
+}
